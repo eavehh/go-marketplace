bce1af25b2cee3118fa42281012a2e488786cb17
Ab <suleimanovv73@icloud.com>
2026-06-11 18:12:34 +0300

    created handler api that returns all the brands

---
+++ internal/catalog/api/handlers/brands_handler.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/eavehh/marketpl.microserv/internal/catalog/application/queries"
+	"github.com/gin-gonic/gin"
+)
+
+type Brands_handler struct {
+	brands *queries.Brands_handler
+}
+
+func New_Brands_handler(brands *queries.Brands_handler) *Brands_handler {
+	return &Brands_handler{brands: brands}
+}
+
+func (h *Brands_handler) Brands(c *gin.Context) {
+	brands, err := h.brands.Handle(c.Request.Context())
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"brands": brands,
+	})
+}
