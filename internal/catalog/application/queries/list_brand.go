ec3ade88027504c79f07d0ce09fed5d7e06d5773
Ab <suleimanovv73@icloud.com>
2026-06-11 18:11:46 +0300

    created query that return the list of all brands to higher level (handler api)

---
+++ internal/catalog/application/queries/list_brand.go
@@ -0,0 +1,20 @@
+package queries
+
+import (
+	"context"
+
+	"github.com/eavehh/marketpl.microserv/internal/catalog/domain/entity"
+	"github.com/eavehh/marketpl.microserv/internal/catalog/domain/repositories"
+)
+
+type Brands_handler struct {
+	repo repositories.Brand_repo
+}
+
+func New_brand_handler(repo repositories.Brand_repo) *Brands_handler {
+	return &Brands_handler{repo: repo}
+}
+
+func (q *Brands_handler) Handle(ctx context.Context) ([]entity.Brand, error) {
+	return q.repo.Brands(ctx)
+}
